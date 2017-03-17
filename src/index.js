579c04ecb301133c865a0d609ae8fc92545d8acd
prescottprue <sprue.dev@gmail.com>
2017-03-16 22:52:07 -0700

    Code split into logger and commands utils.

--- src/index.js
+++ src/index.js
@@ -1,72 +1,16 @@
 /* eslint-disable no-console */
 import chalk from 'chalk'
-import { isUndefined, reduce } from 'lodash'
-const exec = require('child_process').exec
-const {
-  TRAVIS_BRANCH,
-  TRAVIS_PULL_REQUEST,
-  FIREBASE_TOKEN
-} = process.env
+import { isUndefined } from 'lodash'
+import { createCommandsPromise } from './utils/commands'
+import { info, warn } from './utils/logger'
 
-const skipPrefix = 'Skipping Firebase Deploy'
+const { TRAVIS_BRANCH, TRAVIS_PULL_REQUEST, FIREBASE_TOKEN } = process.env
+const skipPrefix = 'firebase-ci: Skipping Firebase Deploy'
 const branchWhitelist = [
   'master',
   'stage',
   'prod'
 ]
-const isPromise = (obj) => {
-  return obj && typeof obj.then === 'function'
-}
-
-/**
- * @description Run a bash command using exec.
- * @param {Object} opts - Options object
- * @param {Object} opts.command - Command to be executed
- * @param {Object} opts.before - Before callback
- * @param {Object} opts.error - Error callback
- * @param {Object} opts.success - Success Callback
- * @private
- */
-const runCommand = ({ command, before, error, success }) => {
-  console.log('run command called:', { command, before })
-  if (before) {
-    before()
-  }
-  return new Promise((resolve, reject) => {
-    exec(command, (err, stdout) => {
-      if (err !== null) {
-        if (error) {
-          error(err, null)
-        }
-        reject(err)
-      } else {
-        console.log(stdout) // log output
-        if (success) {
-          success(stdout)
-        }
-        resolve(stdout)
-      }
-    })
-  })
-}
-
-/**
- * @description Create a promise that runs commands in waterfall
- * @private
- */
-const createCommandsPromise = (commands) => {
-  console.log('commands', commands)
-  return reduce(commands, (l, r) => {
-    console.log('in reduce:', { l, r, commands })
-    if (!isPromise(l)) {
-      return runCommand(r)
-    }
-    if (isPromise(runCommand(r))) {
-      return l.then(runCommand(r))
-    }
-    return l
-  }, [])
-}
 
 /**
  * @description Deploy to Firebase under specific conditions
@@ -74,9 +18,10 @@ const createCommandsPromise = (commands) => {
  * @private
  */
 const deployToFirebase = ({ only }, cb) => {
+  // // TODO: Support other CI environments
   if (isUndefined(TRAVIS_BRANCH)) {
     const nonCiMessage = `${skipPrefix} - Not a supported CI environment`
-    console.log(chalk.blue(nonCiMessage))
+    warn(nonCiMessage)
     if (cb) {
       return cb(null, nonCiMessage)
     }
@@ -85,64 +30,54 @@ const deployToFirebase = ({ only }, cb) => {
 
   if (!!TRAVIS_PULL_REQUEST && TRAVIS_PULL_REQUEST !== 'false') {
     const pullRequestMessage = `${skipPrefix} - Build is a Pull Request`
-    console.log(chalk.blue(pullRequestMessage))
-    if (cb) {
-      return cb(null, pullRequestMessage)
-    }
-    return
+    info(pullRequestMessage)
+    return cb && cb(null, pullRequestMessage)
   }
 
   if (branchWhitelist.indexOf(TRAVIS_BRANCH) === -1) {
-    const nonBuildBranch = `${skipPrefix} - Build is a not a Build Branch - Branch: ${TRAVIS_BRANCH}`
-    console.log(chalk.blue(nonBuildBranch))
-    if (cb) {
-      return cb(null, nonBuildBranch)
-    }
-    return
+    const nonBuildBranch = `${skipPrefix} - not a Build Branch`
+    info(nonBuildBranch)
+    return cb && cb(null, nonBuildBranch)
   }
 
   if (!FIREBASE_TOKEN) {
-    console.log(chalk.blue('Error: FIREBASE_TOKEN env variable not found.\n' +
-      'Run firebase login:ci (from  firebase-tools) to generate a token' +
-      'and place it travis environment variables as FIREBASE_TOKEN'
-    ))
-    cb('Error: FIREBASE_TOKEN env variable not found', null)
-    return
+    console.log(`${chalk.bold.red('firebase-ci Error:')} ${chalk.bold('FIREBASE_TOKEN')} environment variable not found.\n` +
+      'You must generate a token:\n' +
+      '\t1. Make sure you have firebase-tools installed (npm install -g firebase-tools)\n' +
+      '\t2. Run firebase login:ci\n' +
+      `\t3. to generate a token and place it CI environment variables as ${chalk.bold('FIREBASE_TOKEN')}\n`
+    )
+    return cb && cb('Error: FIREBASE_TOKEN env variable not found', null)
   }
+
   const onlyString = only ? `--only ${only}` : ''
-  const project = TRAVIS_BRANCH
+  const tokenString = FIREBASE_TOKEN ? `--token ${FIREBASE_TOKEN}` : ''
+
+  // TODO: Allow custom input of project
+  const project = TRAVIS_BRANCH || 'master'
 
   const commands = [
     {
       command: 'npm i -g firebase-tools',
-      before: () => {
-        console.log(chalk.blue('Installing firebase-tools...'))
-      },
-      success: (stdout) => {
-        console.log(chalk.green('firebase-tools installed successfully'))
-      },
-      error: (error) => {
-        console.log(chalk.red('Error installing firebase:\n', error.toString() || error))
-      }
+      beforeMsg: 'Installing firebase-tools...',
+      successMsg: 'firebase-tools installed successfully',
+      errorMsg: 'Error installing firebase-tools'
     },
     {
-      command: `firebase deploy ${onlyString} --token ${FIREBASE_TOKEN} --project ${project}`,
-      before: () => {
-        console.log(chalk.blue('Deploying to Firebase...'))
-      },
-      success: (stdout) => {
-        console.log(chalk.green(`Successfully Deployed to ${project}`))
-      },
-      error: (error) => {
-        console.log(chalk.red('Error installing firebase:\n', error.toString() || error))
-      }
+      command: `firebase deploy ${onlyString} ${tokenString} --project ${project}`,
+      beforeMsg: 'Deploying to Firebase...',
+      successMsg: `Successfully Deployed to ${project}`,
+      errorMsg: 'Error deploying to Firebase'
     }
   ]
 
   const commandsPromise = createCommandsPromise(commands)
 
   commandsPromise.then(() => {
-    console.log('commands executed successfully')
+    cb && cb(null, 'Success!')
+  })
+  .catch((err) => {
+    cb && cb(err)
   })
 }
 
---
+++ src/utils/logger.js
@@ -0,0 +1,21 @@
+import chalk from 'chalk'
+
+const colorMapping = {
+  info: 'blue',
+  warn: 'yellow',
+  success: 'green',
+  error: 'red'
+}
+
+const logType = (type, message, other) => {
+  console.log(chalk[type](message)) // eslint-disable-line no-console
+  if (other) {
+    console.log('\n', other) // eslint-disable-line no-console
+  }
+}
+
+export const log = console.log // eslint-disable-line
+export const info = (message, other) => logType(colorMapping.info, message, other)
+export const success = (message, other) => logType(colorMapping.success, message, other)
+export const warn = (message, other) => logType(colorMapping.warn, message, other)
+export const error = (message, other) => logType(colorMapping.error, message, other)
