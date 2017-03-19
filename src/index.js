6eebca9135cd45035c41941da1ae5920a0f24853
prescottprue <sprue.dev@gmail.com>
2017-03-19 16:06:45 -0700

    Went back to using commands not wrapped in promises.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-console */
 import chalk from 'chalk'
 import { isUndefined } from 'lodash'
-import { createCommandsPromise } from './utils/commands'
-import { info, warn } from './utils/logger'
+const exec = require('child_process').exec
+const {
+  TRAVIS_BRANCH,
+  TRAVIS_PULL_REQUEST,
+  FIREBASE_TOKEN
+} = process.env
 
-const { TRAVIS_BRANCH, TRAVIS_PULL_REQUEST, FIREBASE_TOKEN } = process.env
-const skipPrefix = 'firebase-ci: Skipping Firebase Deploy'
+const skipPrefix = 'Skipping Firebase Deploy'
 const branchWhitelist = [
   'master',
   'stage',
@@ -18,67 +21,70 @@ const branchWhitelist = [
  * @private
  */
 const deployToFirebase = ({ only }, cb) => {
-  // // TODO: Support other CI environments
   if (isUndefined(TRAVIS_BRANCH)) {
     const nonCiMessage = `${skipPrefix} - Not a supported CI environment`
-    warn(nonCiMessage)
+    console.log(chalk.blue(nonCiMessage))
     if (cb) {
       return cb(null, nonCiMessage)
     }
     return
   }
-
   if (!!TRAVIS_PULL_REQUEST && TRAVIS_PULL_REQUEST !== 'false') {
     const pullRequestMessage = `${skipPrefix} - Build is a Pull Request`
-    info(pullRequestMessage)
-    return cb && cb(null, pullRequestMessage)
+    console.log(chalk.blue(pullRequestMessage))
+    if (cb) {
+      return cb(null, pullRequestMessage)
+    }
+    return
   }
 
   if (branchWhitelist.indexOf(TRAVIS_BRANCH) === -1) {
-    const nonBuildBranch = `${skipPrefix} - not a Build Branch`
-    info(nonBuildBranch)
-    return cb && cb(null, nonBuildBranch)
+    const nonBuildBranch = `${skipPrefix} - Build is a not a Build Branch - Branch: ${TRAVIS_BRANCH}`
+    console.log(chalk.blue(nonBuildBranch))
+    if (cb) {
+      return cb(null, nonBuildBranch)
+    }
+    return
   }
 
   if (!FIREBASE_TOKEN) {
-    console.log(`${chalk.bold.red('firebase-ci Error:')} ${chalk.bold('FIREBASE_TOKEN')} environment variable not found.\n` +
-      'You must generate a token:\n' +
-      '\t1. Make sure you have firebase-tools installed (npm install -g firebase-tools)\n' +
-      '\t2. Run firebase login:ci\n' +
-      `\t3. to generate a token and place it CI environment variables as ${chalk.bold('FIREBASE_TOKEN')}\n`
-    )
-    return cb && cb('Error: FIREBASE_TOKEN env variable not found', null)
+    console.log(chalk.blue('Error: FIREBASE_TOKEN env variable not found.\n' +
+      'Run firebase login:ci (from  firebase-tools) to generate a token' +
+      'and place it travis environment variables as FIREBASE_TOKEN'
+    ))
+    cb('Error: FIREBASE_TOKEN env variable not found', null)
+    return
   }
 
+  console.log(chalk.blue('Installing firebase-tools...'))
+
   const onlyString = only ? `--only ${only}` : ''
-  const tokenString = FIREBASE_TOKEN ? `--token ${FIREBASE_TOKEN}` : ''
-
-  // TODO: Allow custom input of project
-  const project = TRAVIS_BRANCH || 'master'
-
-  const commands = [
-    {
-      command: 'npm i -g firebase-tools',
-      beforeMsg: 'Installing firebase-tools...',
-      successMsg: 'firebase-tools installed successfully',
-      errorMsg: 'Error installing firebase-tools'
-    },
-    {
-      command: `firebase deploy ${onlyString} ${tokenString} --project ${project}`,
-      beforeMsg: 'Deploying to Firebase...',
-      successMsg: `Successfully Deployed to ${project}`,
-      errorMsg: 'Error deploying to Firebase'
+  const project = TRAVIS_BRANCH
+  exec(`npm i -g firebase-tools`, (error, stdout) => {
+    if (error !== null) {
+      console.log(chalk.red('Error deploying to firebase.', error.toString() || error))
+      if (cb) {
+        cb(error, null)
+        return
+      }
     }
-  ]
-
-  const commandsPromise = createCommandsPromise(commands)
-
-  commandsPromise.then(() => {
-    cb && cb(null, 'Success!')
-  })
-  .catch((err) => {
-    cb && cb(err)
+    console.log(stdout) // log output
+    console.log(chalk.green('firebase-tools installed successfully'))
+    console.log(chalk.blue('Deploying to Firebase...'))
+    exec(`firebase deploy ${onlyString} --token ${FIREBASE_TOKEN} --project ${project}`, (error, stdout) => {
+      if (error !== null) {
+        console.log(chalk.red('Error deploying to firebase: '), error.toString() || error)
+        if (cb) {
+          cb(error, null)
+          return
+        }
+      }
+      console.log(stdout) // log output
+      console.log(chalk.green(`Successfully Deployed to ${project}`))
+      if (cb) {
+        cb(null, stdout)
+      }
+    })
   })
 }
-
 export default deployToFirebase
