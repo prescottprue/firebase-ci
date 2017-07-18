52f58e8d485227082a777a93b0659c55907b5410
Scott Prue <scott@residenetwork.com>
2017-07-18 16:51:28 -0700

    Not throwing error for missing firebase.json.

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ const getFile = (filePath) => {
   const localPath = path.join(process.cwd(), filePath)
   if (!fs.existsSync(localPath)) {
     console.log(chalk.red(`${filePath} file does not exist!`), ' Run firebase init to create.')
-    throw new Error(`${filePath} file does not exist! Run firebase init to create.`)
+    // throw new Error(`${filePath} file does not exist! Run firebase init to create.`)
+    return {}
   }
 
   try {
