edb84470890ba9a69fbada89fe5ab4817b754da1
prescottprue <sprue.dev@gmail.com>
2018-07-28 22:11:14 -0700

    Add fallback to environment name if CI_COMMIT_REF_SLUG does not match a project in .firebaserc

--- src/actions/deploy.js
+++ src/actions/deploy.js
@@ -1,4 +1,4 @@
-import { isUndefined, compact } from 'lodash'
+import { isUndefined, compact, get } from 'lodash'
 import { getFile, functionsExists } from '../utils/files'
 import { error, info, warn } from '../utils/logger'
 import { runCommand, shellescape } from '../utils/commands'
@@ -15,7 +15,9 @@ const {
   FIREBASE_TOKEN,
   CI,
   CI_COMMIT_MESSAGE,
-  CI_ENVIRONMENT_SLUG
+  CI_COMMIT_REF_SLUG,
+  CI_ENVIRONMENT_SLUG,
+  CIRCLE_SHA1
 } = process.env
 
 const skipPrefix = 'Skipping Firebase Deploy'
@@ -82,27 +84,30 @@ export default opts => {
     return Promise.reject(new Error('firebase.json file is required'))
   }
 
-  if (
-    settings.projects &&
-    !settings.projects[TRAVIS_BRANCH || CIRCLE_BRANCH || CI_ENVIRONMENT_SLUG]
-  ) {
-    const nonBuildBranch = `${skipPrefix} - Branch is not a project alias - Branch: ${TRAVIS_BRANCH ||
-      CIRCLE_BRANCH}`
-    info(nonBuildBranch)
-    return Promise.resolve(nonBuildBranch)
-  }
-
+  const branchName = TRAVIS_BRANCH || CIRCLE_BRANCH || CI_COMMIT_REF_SLUG
+  const fallbackProjectName = CI_ENVIRONMENT_SLUG
+  // Get project from passed options, falling back to branch name
+  const projectName = get(opts, 'project', branchName)
+  // Get project setting from settings file based on branchName falling back
+  // to fallbackProjectName
+  const projectSetting = get(settings, `projects.${projectName}`)
+  const fallbackProjectSetting = get(
+    settings,
+    `projects.${fallbackProjectName}`
+  )
   // Handle project option
-  if (
-    opts &&
-    opts.project &&
-    settings.projects &&
-    !settings.projects[opts.project]
-  ) {
-    const nonProjectBranch = `${skipPrefix} - Project is a not an Alias - Project: ${
-      opts.project
-    }`
+  if (!projectSetting) {
+    const nonProjectBranch = `${skipPrefix} - Project is a not an Alias - ${
+      opts.project ? 'Project' : 'Branch'
+    }: ${projectName}, checking for fallback...`
     info(nonProjectBranch)
+    if (!fallbackProjectSetting) {
+      const nonFallbackBranch = `${skipPrefix} - Fallback Project is a not an Alias - ${
+        opts.project ? 'Project' : 'Branch'
+      }: ${fallbackProjectName} exiting...`
+      info(nonFallbackBranch)
+      return Promise.resolve(nonProjectBranch)
+    }
     return Promise.resolve(nonProjectBranch)
   }
 
@@ -117,14 +122,11 @@ export default opts => {
     )
   }
 
-  const originalMessage = TRAVIS_COMMIT_MESSAGE || CI_COMMIT_MESSAGE
+  const originalMessage =
+    TRAVIS_COMMIT_MESSAGE || CIRCLE_SHA1 || CI_COMMIT_MESSAGE
 
   const onlyString = opts && opts.only ? `--only ${opts.only}` : ''
-  const project =
-    TRAVIS_BRANCH ||
-    CIRCLE_BRANCH ||
-    CI_ENVIRONMENT_SLUG ||
-    settings.projects.default
+  const project = branchName || settings.projects.default
   // // First 300 characters of travis commit message or "Update"
   const message = originalMessage
     ? originalMessage.replace(/"/g, "'").substring(0, 300)
