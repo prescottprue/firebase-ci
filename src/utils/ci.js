dfdbf253af141efca7a1c739e912f86c04cb1837
kde713 <kde713@gmail.com>
2018-08-10 18:56:17 +0900

    Added Support for Bitbucket Pipeline (deploy)

--- src/utils/ci.js
+++ src/utils/ci.js
@@ -1,9 +1,10 @@
 export const getBranch = () => {
-  const {TRAVIS_BRANCH, CIRCLE_BRANCH} = process.env
-  return TRAVIS_BRANCH || CIRCLE_BRANCH
+  const {TRAVIS_BRANCH, CIRCLE_BRANCH, BITBUCKET_BRANCH} = process.env
+  return TRAVIS_BRANCH || CIRCLE_BRANCH || BITBUCKET_BRANCH
 }
 
 export const isPullRequest = () => {
+  // Currently Bitbucket pipeline doesn't support build for PR. So doesn't include in this function.
   const {TRAVIS_PULL_REQUEST, CIRCLE_PR_NUMBER} = process.env
   return (!!TRAVIS_PULL_REQUEST && TRAVIS_PULL_REQUEST !== 'false') || (!!CIRCLE_PR_NUMBER && CIRCLE_PR_NUMBER !== 'false')
 }
