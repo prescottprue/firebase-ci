646a94ca338187147f18f9896570a61f57439edc
prescottprue <sprue.dev@gmail.com>
2018-08-14 00:52:14 -0700

    v0.5.0-alpha.5

--- src/utils/deps.js
+++ src/utils/deps.js
@@ -1,33 +1,48 @@
-import commandExists from 'command-exists'
 import { functionsExists, functionsNodeModulesExist } from './files'
 import { runCommand } from './commands'
-import { info as logInfo } from './logger'
-
-const commandExistsSync = commandExists.sync
+import { info as logInfo, error } from './logger'
 
 /**
  * Install Firebase tools and run npm install in functions folder
- * @return {[type]} [description]
+ * @return {Promise} Resolves when dependencies have been installed
  */
 export async function installDeps(opts = {}, settings = {}) {
   const { info } = opts
   const { toolsVersion } = settings
   const versionSuffix = toolsVersion ? `@${toolsVersion}` : ''
-  const promises = []
-  logInfo('Checking to see if firebase-tools already exists...')
-  if (!commandExistsSync('firebase')) {
-    if (!settings.skipToolsInstall) {
-      promises.push(
-        runCommand({
-          command: `npm i firebase-tools${versionSuffix} ${info ? '' : '-q'}`,
-          beforeMsg: 'firebase-tools does not already exist, installing...',
-          errorMsg: 'Error installing firebase-tools.',
-          successMsg: 'Firebase tools installed successfully!'
-        })
-      )
+  // Check version of firebase tools using npx (to allow for locally and
+  // globally installed versions of firebase-tools)
+  const fbVersion = await runCommand({
+    command: 'npx',
+    args: ['firebase', '--version'],
+    pipeOutput: false,
+    beforeMsg: 'Checking to see if firebase-tools is installed...',
+    errorMsg: 'Error checking for firebase-tools.'
+  })
+  // Skip installing firebase-tools if specified by config
+  if (settings.skipToolsInstall) {
+    // Throw for missing version when skipping install
+    if (!fbVersion) {
+      const missingFbTools =
+        'firebase-tools install skipped, and no existing version found!'
+      error(missingFbTools)
+      throw new Error(missingFbTools)
     }
+    logInfo(`Installing of firebase-tools skipped based on config settings.`)
   } else {
-    logInfo('firebase-tools already exists')
+    // Log version of firebase-tools if it exists, otherwise install
+    if (fbVersion) {
+      logInfo(`firebase-tools already exists, version: ${fbVersion}`)
+    } else {
+      // Install firebase-tools using npm
+      await runCommand({
+        command: 'npm',
+        args: ['i', `firebase-tools${versionSuffix}`, `${info ? '' : '-q'}`],
+        beforeMsg: 'firebase-tools does not already exist, installing...',
+        errorMsg: 'Error installing firebase-tools.',
+        successMsg: 'Firebase tools installed successfully!'
+      })
+    }
   }
   // Call npm install in functions folder if it exists and does
   // not already contain node_modules
@@ -36,14 +51,11 @@ export async function installDeps(opts = {}, settings = {}) {
     !functionsNodeModulesExist() &&
     !settings.skipFunctionsInstall
   ) {
-    promises.push(
-      runCommand({
-        command: `npm i --prefix functions`,
-        beforeMsg: 'Running npm install in functions folder...',
-        errorMsg: 'Error installing functions dependencies.',
-        successMsg: 'Functions dependencies installed successfully!'
-      })
-    )
+    await runCommand({
+      command: `npm i --prefix functions`,
+      beforeMsg: 'Running npm install in functions folder...',
+      errorMsg: 'Error installing functions dependencies.',
+      successMsg: 'Functions dependencies installed successfully!'
+    })
   }
-  return Promise.all(promises)
 }
